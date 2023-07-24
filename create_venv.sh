32267299f2c0f8e6a814f4de248b14b071b9f10e
Veniamin-Arefev <veniamin.arefev@mail.ru>
2023-07-24 16:48:14 +0300

    Done script task

--- create_venv.sh
+++ create_venv.sh
@@ -1,5 +1,4 @@
 python3 -m venv venv
 . venv/bin/activate
-pip install pyyaml==5.3.1 opera ansible
+pip install pyyaml==5.3.1 opera ansible pyjokes googletrans==3.1.0a0 requests
 
-ansible-galaxy collection install community.libvirt
---
+++ test_this.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+
+import json
+import subprocess
+import html
+
+import requests
+import pyjokes
+from googletrans import Translator
+
+url = "http://192.168.122.55"
+
+def get_joke() -> str:
+  translator = Translator()
+  joke = pyjokes.get_joke()
+  joke_result = translator.translate(joke, dest='ru')
+  return [joke, joke_result.text]
+
+def start_opera():    
+  command_deploy = ["opera deploy service.yaml -i ins.json -v"]
+  
+  p = subprocess.run(command_deploy, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  with open('deploy.logs', 'w') as file:
+    file.write(p.stdout)
+
+def stop_opera():    
+  command_undeploy = ["opera undeploy"]
+
+  p = subprocess.run(command_undeploy, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+  with open('undeploy.logs', 'w') as file:
+    file.write(p.stdout)
+
+joke_texts = get_joke()
+
+get_text = '\n\n'.join(joke_texts)
+print(f"Generated :\n{get_text}")
+
+html_text = "<br>".join(map(lambda x: f"<div>{x}</div>", joke_texts))
+with open('ins.json', 'w') as file:
+    json.dump({"site_text":html_text}, file)
+ 
+start_opera()
+
+r = requests.get(url)
+
+r.encoding = 'utf-8'
+
+print(f"Anekdot is in docker: {all([anek in html.unescape(r.text) for anek in joke_texts])}")
+
+#print(html.unescape(r.text))
+
+input(f"You can check youself: {url}"+"\nWhen you are done, press enter")
+
+stop_opera()
+
+
+
+
+
+
+
+
+
